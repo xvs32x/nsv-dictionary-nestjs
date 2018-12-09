2f1ece271297831b2f277dfc71c4cd1fd4ad0dbf
NagSer <x64vs32x@mail.ru>
2018-12-09 19:35:13 +0300

    Change host

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,10 @@ const serverName = 'heroku';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'us-cdbr-east.cleardb.com',
+      host: 'eu-cdbr-west-02.cleardb.net',
       port: 3306,
-      username: 'b5868a056b7e4c',
-      password: 'e744bff2',
+      username: 'b5868a' + '056b7e4c',
+      password: 'e744' + 'bff2',
       database: serverName + '_e185ea72ecb7e1b',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
