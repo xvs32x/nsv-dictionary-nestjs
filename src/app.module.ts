f414183377992757b7b72d5fe647a5ce30abe725
NagSer <x64vs32x@mail.ru>
2018-12-16 18:53:01 +0300

    Fix env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,16 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WordEntity } from './word.entity';
 const serverName = 'heroku';
-// process.env.NODE_ENV === 'development'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'eu-cdbr-west-02.cleardb.net',
+      host: process.env.NODE_ENV === 'development' ? 'localhost' : 'eu-cdbr-west-02.cleardb.net',
       port: 3306,
-      username: 'b5868a' + '056b7e4c',
-      password: 'e744' + 'bff2',
-      database: serverName + '_e185ea72ecb7e1b',
+      username: process.env.NODE_ENV === 'development' ? 'root' : ('b5868a' + '056b7e4c'),
+      password: process.env.NODE_ENV === 'development' ? '' : ('e744' + 'bff2'),
+      database: process.env.NODE_ENV === 'development' ? 'words' : (serverName + '_e185ea72ecb7e1b'),
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
